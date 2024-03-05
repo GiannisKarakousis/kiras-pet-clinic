7f79d5123b3a551f963b84fc17028fbdb685a4ce
GiannisKarakousis <johnpao123@hotmail.com>
2024-03-05 12:37:14 +0200

    added spring data jpa services for petType. closes #42

---
+++ pet-clinic-data/src/main/java/guru/springframework/kiraspetclinic/services/springdatajpa/PetTypeSDJpaService.java
@@ -0,0 +1,46 @@
+package guru.springframework.kiraspetclinic.services.springdatajpa;
+
+import guru.springframework.kiraspetclinic.model.PetType;
+import guru.springframework.kiraspetclinic.repositories.PetTypeRepository;
+import guru.springframework.kiraspetclinic.services.PetTypeService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PetTypeSDJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeSDJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+
+        petTypeRepository.findAll().forEach(petTypes::add);
+
+        return petTypes;
+    }
+
+    @Override
+    public PetType fintById(Long id) {
+        return petTypeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petTypeRepository.deleteById(id);
+    }
+}
