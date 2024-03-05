50ba11f56cf2d24658ce2ffdbc3a71f36ce3ceb5
GiannisKarakousis <johnpao123@hotmail.com>
2024-03-05 12:42:42 +0200

    added spring data jpa services for pet. closes #43

--- pet-clinic-data/src/main/java/guru/springframework/kiraspetclinic/services/springdatajpa/PetTypeSDJpaService.java
+++ pet-clinic-data/src/main/java/guru/springframework/kiraspetclinic/services/springdatajpa/PetTypeSDJpaService.java
@@ -3,10 +3,14 @@ package guru.springframework.kiraspetclinic.services.springdatajpa;
 import guru.springframework.kiraspetclinic.model.PetType;
 import guru.springframework.kiraspetclinic.repositories.PetTypeRepository;
 import guru.springframework.kiraspetclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
+@Profile("springdatajpa")
 public class PetTypeSDJpaService implements PetTypeService {
 
     private final PetTypeRepository petTypeRepository;
